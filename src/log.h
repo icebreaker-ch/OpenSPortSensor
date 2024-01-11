0c03f704d2259ad3d646f0e435118796dbdd66af
icebreaker-ch <roland.schaeuble@gmail.com>
2024-01-11 18:34:20 +0100

    Added Logging Service

---
+++ src/log.h
@@ -0,0 +1,25 @@
+#ifndef LOG_H
+#define LOG_H
+
+#ifndef PLATFORM_NATIVE
+#include <Arduino.h>
+#endif
+
+//#define LOGGING_ON
+
+#ifdef LOGGING_ON
+    static void printLog () {
+    }
+
+    template<class T, class... Types>
+    void printLog (T firstArg, Types... args) {
+        Serial.print(firstArg); // printing first argument
+        printLog(args...); // printing the rest of the arguments recursive
+    }
+
+    #define LOG(...) printLog(__FILE__, ":", __LINE__, ": ", __VA_ARGS__)
+#else
+    #define LOG(...)
+#endif
+
+#endif
